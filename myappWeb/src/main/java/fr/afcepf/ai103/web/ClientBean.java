88eb8c4d8686e8319a579af6587336b922c2839c
didier <didier@ici_ou_la.fr>
2018-07-11 11:52:16 +0200

    nouvelle version

--- myappWeb/src/main/java/fr/afcepf/ai103/web/ClientBean.java
+++ myappWeb/src/main/java/fr/afcepf/ai103/web/ClientBean.java
@@ -4,16 +4,19 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import fr.afcepf.ai103.data.Client;
+import fr.afcepf.ai103.service.ServiceClient;
 
 @ManagedBean
 @SessionScoped
 public class ClientBean {
-    private Long numClient;
-    private String password;
+    private Long numClient; //à saisir
+    private String password; //à saisir
     
-    private String message;
+    private String message; //à afficher
     
     private Client client; //infos "client" à récupérer
+    
+    private ServiceClient serviceClient = new ServiceClient();
     
     public String verifLogin() {
     	String suite=null; /* si suite reste à null on reste sur même page */
@@ -22,6 +25,7 @@ public class ClientBean {
     		//mot de passe considéré comme ok si "pwd" + numClient (ex: "pwd1" )
     		//on demande à naviguer vers la page client
     		suite = "client"; //.jsf (.jsp ou .xhtml)
+    		this.client  =  serviceClient.rechercherInfosClient(numClient);
     	}else {
     		message = "invalid password"; 
     	}
