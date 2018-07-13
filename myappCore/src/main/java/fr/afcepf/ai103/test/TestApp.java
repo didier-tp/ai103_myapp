7f8f996911250d451b68f20cbf556ee5cf4db654
didier <didier@ici_ou_la.fr>
2018-07-13 14:19:03 +0200

    nouvelle version

---
+++ myappCore/src/main/java/fr/afcepf/ai103/test/TestApp.java
@@ -0,0 +1,17 @@
+package fr.afcepf.ai103.test;
+
+import fr.afcepf.ai103.dao.DaoClientJpa;
+import fr.afcepf.ai103.dao.IDaoClient;
+import fr.afcepf.ai103.data.Client;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		IDaoClient daoClient =  new DaoClientJpa();
+		for(Client c :daoClient.rechercherClients()) {
+			System.out.println(c.toString());
+		}
+		//..
+	}
+
+}
